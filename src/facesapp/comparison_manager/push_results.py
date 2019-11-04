7a79ed83a2ad1fe0719e7b9f68795bd959e20efc
Waqar Akbar <waqarakbar89@gmail.com>
2019-11-04 15:27:31 +0500

    pip3 requriements requests library

--- src/facesapp/comparison_manager/push_results.py
+++ src/facesapp/comparison_manager/push_results.py
@@ -1,5 +1,8 @@
 from django.http import JsonResponse
 from ..models import Image as ImageModel, Comparison
+import requests
+import json
+from django.conf import settings
 
 def push_results(request):
     # push the result from comparisons table to App server
@@ -15,20 +18,32 @@ def push_results(request):
         this_match = {
             'comparison_id': match.id,
             'image_id': match.image.id,
-            'attachment_source': match.image.source,
+            'attachment_source_id': match.image.source,
             'source_entity_id': match.image.source_id,
             'attachment_id': match.image.attachment_id,
             'matched_image_id': match.compared_image.id,
-            'matched_attachment_source': match.compared_image.source,
+            'matched_attachment_source_id': match.compared_image.source,
             'matched_source_entity_id': match.compared_image.source_id,
             'matched_attachment_id': match.compared_image.attachment_id,
             'face_distance': match.face_distance
         }
-        #print(this_match)
+        # print(this_match)
         matches_list.append(this_match)
 
+        # change the status
+        match.sent_back = 1
+        match.save()
 
-    return JsonResponse(matches_list, status=200, safe=False)
+
+    # push the results to MBAlert server
+    url = settings.PUSH_MATCHES_URL
+    headers = {'Content-Type': 'application/json', 'Token': settings.MBALERT_SERVER_TOKEN}
+    r = requests.post(url, data=json.dumps(matches_list), headers=headers)
+    # print(r.text)
+    # print(matches_list)
+
+    # return JsonResponse(matches_list, status=200, safe=False)
     return JsonResponse({
-        'message': 'Pending matches sent to server'
+        'message': 'Pending matches sent to server',
+        'response': r.text
     }, status=200)
